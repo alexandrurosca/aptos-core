5c7bc57a6b6c85f37c14d3df37457c1cb57db01b
Rosca Alexandru <alex.rosca1996@gmail.com>
2023-02-20 15:41:45 +0200

    create new files

---
+++ aptos-move/move-examples/tiexo-marketplace/sources/marketplace_v1.move
@@ -0,0 +1,130 @@
+/// This is an example demonstrating how to use marketplace_bid_utils and market_place_listing_utils to build an auction house
+/// This example shows how to build a decentralized marketplace where listing are stored under owner's account
+/// Note: the buyer can buy from any listing that is stored under owners' account
+/// For more detailed description, check readme
+module marketplace::marketplace_v1 {
+    use std::signer;
+    use std::string::String;
+    use aptos_std::table::Table;
+    use marketplace::marketplace_listing_utils::{Self as listing_utils, Listing};
+    use marketplace::marketplace_bid_utils::{Self as bid_utils};
+    use aptos_framework::timestamp;
+
+    use aptos_framework::guid::ID;
+
+
+    struct Config has key {
+        market_fee_numerator: u64,
+        market_fee_denominator: u64,
+        fee_address: address,
+    }
+
+    public entry fun initialize_market(
+        account: &signer,
+        market_fee_numerator: u64,
+        market_fee_denominator: u64,
+        fee_address: address,
+    ) {
+        move_to(
+            account,
+            Config {
+                market_fee_denominator,
+                market_fee_numerator,
+                fee_address,
+            }
+        );
+    }
+
+     public entry fun update_market_config(
+        account: &signer,
+        market_fee_numerator: u64,
+        market_fee_denominator: u64,
+        fee_address: address,
+    ) acquires Config {
+        // todo: add checks
+        let account_addr = signer::address_of(account);
+        let configRef = borrow_global_mut<Config>(account_addr);
+        // let market_fee_denominatorRef = &mut borrow_global_mut<Config>(signer::address_of(account)).market_fee_denominator;
+        // let fee_addressRef = &mut borrow_global_mut<Config>(signer::address_of(account)).fee_address;
+
+
+        configRef.market_fee_denominator = market_fee_denominator;
+
+        configRef.market_fee_numerator = market_fee_numerator;
+
+        configRef.fee_address = fee_address;
+    }
+
+    struct Listings<phantom CoinType> has key {
+        all_active_Listings: Table<ID, Listing<CoinType>>,
+    }
+
+    public entry fun create_listing<CoinType>(
+        owner: &signer,
+        creator: address,
+        collection_name: String,
+        token_name: String,
+        property_version: u64,
+        // amount: u64,
+        min_price: u64,
+        // start_sec: u64,
+        // expiration_sec: u64,
+        // withdraw_expiration_sec: u64,
+    ) {
+        let expiration_sec = timestamp::now_seconds() + 157680000; // 5 years
+        listing_utils::direct_listing<CoinType>(
+            owner,
+            creator,
+            collection_name,
+            token_name,
+            property_version,
+            1, // amount
+            min_price,
+            true,
+            0, // start_sec
+            expiration_sec, // expiration_sec
+            expiration_sec + 1, // withdraw_expiration_sec
+        );
+    }
+
+    public entry fun remove_listing<CoinType>(
+        owner: &signer,
+        listing_creation_number: u64,
+    ) {
+        listing_utils::cancel_direct_listing<CoinType>(
+            owner,
+            listing_creation_number
+        );
+    }
+
+    public entry fun change_listing<CoinType>(
+        owner: &signer,
+        listing_creation_number: u64,
+        new_price: u64
+    ) {
+        listing_utils::change_price_listing<CoinType>(
+            owner,
+            listing_creation_number,
+            new_price
+        );
+    }
+
+    public entry fun buy_listing<CoinType>(
+        buyer: &signer,
+        lister_address: address,
+        listing_creation_number: u64,
+    ) acquires Config {
+        // charge fee for the aggregator
+        let config = borrow_global<Config>(@marketplace);
+
+        // buy the token from owner directly
+        bid_utils::buy_from_owner_with_fee<CoinType>(
+            buyer,
+            lister_address,
+            listing_creation_number,
+            config.fee_address,
+            config.market_fee_numerator,
+            config.market_fee_denominator,
+        );
+    }
+}
--- ecosystem/typescript/sdk/examples/typescript/tx-helper.ts
+++ ecosystem/typescript/sdk/examples/typescript/tx-helper.ts
@@ -21,10 +21,15 @@ const {
   TransactionPayload
 } = TxnBuilderTypes;
 
+
+
 /**
  * This code example demonstrates the process of moving test coins from one account to another.
  */
 (async () => {
+  const MARKET = 'c525b9881ca5d1887c8340b06d30a596ea6ec45e0a621d2ed85a5ec63f23f64a'
+  const MARKET_PK = '02889366abe51d51c172e672bc58289b93807ce22183f1610ccbfd8c9d97ab7d'
+
   const client = new AptosClient(NODE_URL);
   const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
   const feeAcc = '0x811bf5b10cfb58280e70995ed0f6e4a52c6f2806bc81eaa39bddeade7a8e13ed'
@@ -32,9 +37,9 @@ const {
 
   // Generates key pair for a new account
   // const account1 = new AptosAccount();
-  // const acc = new AptosAccount(Buffer.from('1b472a81dc906a0ce38977aaf1cc909da8fdabccc491c2b6386e90738a4f7348', 'hex'), 'd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe');
-  const acc = /*seller*/new AptosAccount(Buffer.from('e02d26d9124f1086518bf4d1706505fc2dab6a2930c36c8ec46c3f4ed77eb186', 'hex'), '5964cbf9198fb32e601657b1e31eba60c227c682306b657ac16fb584feefca66');
-  // const acc = /*buyer*/ new AptosAccount(Buffer.from('785aeed5f114b8651dd768980b0fb0e192aeabaa2c1c6b6c341669a12617a8e9', 'hex'), 'fb9431aa8fda0e007a4eb0f8ca8a19b89705753398c75e7b1377f7f9030e8123');
+  // const acc = new AptosAccount(Buffer.from(MARKET_PK, 'hex'), MARKET);
+  // const acc = /*seller*/new AptosAccount(Buffer.from('e02d26d9124f1086518bf4d1706505fc2dab6a2930c36c8ec46c3f4ed77eb186', 'hex'), '5964cbf9198fb32e601657b1e31eba60c227c682306b657ac16fb584feefca66');
+  const acc = /*buyer*/ new AptosAccount(Buffer.from('785aeed5f114b8651dd768980b0fb0e192aeabaa2c1c6b6c341669a12617a8e9', 'hex'), 'fb9431aa8fda0e007a4eb0f8ca8a19b89705753398c75e7b1377f7f9030e8123');
  
   // console.log({acc: seller.address().toString()})
   const token = new TypeTagStruct(StructTag.fromString("0x1::aptos_coin::AptosCoin"));
@@ -44,14 +49,14 @@ const {
   // const entryFunctionPayload = new TransactionPayloadEntryFunction(
   //   EntryFunction.natural(
   //     // Fully qualified module name, `AccountAddress::ModuleName`
-  //     "0xd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe::marketplace_instant_sale_example",
+  //     `${MARKET}::marketplace_v1`,
   //     // Module function
   //     "initialize_market",
   //     // The coin type to transfer
   //     [],
   //     // Arguments for function `transfer`: receiver account address and amount to transfer
   //     [
-  //       BCS.bcsSerializeUint64(50),
+  //       BCS.bcsSerializeUint64(75),
   //       BCS.bcsSerializeUint64(1000),
   //       BCS.bcsToBytes(AccountAddress.fromHex(feeAcc))
   //     ],
@@ -61,7 +66,7 @@ const {
   // const entryFunctionPayload = new TransactionPayloadEntryFunction(
   //   EntryFunction.natural(
   //     // Fully qualified module name, `AccountAddress::ModuleName`
-  //     "0xd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe::marketplace_instant_sale_example",
+  //     `${MARKET}::marketplace_v1`,
   //     // Module function
   //     "create_listing",
   //     // The coin type to transfer
@@ -72,18 +77,18 @@ const {
   //       BCS.bcsSerializeBytes(Buffer.from('Martian Testnet72878')),
   //       BCS.bcsSerializeBytes(Buffer.from('Martian NFT #72878')),
   //       BCS.bcsSerializeUint64(0),
-  //       BCS.bcsSerializeUint64(1),
+  //       // BCS.bcsSerializeUint64(1),
   //       BCS.bcsSerializeUint64(35000000),
-  //       BCS.bcsSerializeUint64(0),
-  //       BCS.bcsSerializeUint64(10000000000000),
-  //       BCS.bcsSerializeUint64(10000000000001),
+  //       // BCS.bcsSerializeUint64(0),
+  //       // BCS.bcsSerializeUint64(10000000000000),
+  //       // BCS.bcsSerializeUint64(10000000000001),
   //     ],
   //   ),
   // );
   const entryFunctionPayload = new TransactionPayloadEntryFunction(
     EntryFunction.natural(
       // Fully qualified module name, `AccountAddress::ModuleName`
-      "0xd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe::marketplace_instant_sale_example",
+      `${MARKET}::marketplace_v1`,
       // Module function
       "buy_listing",
       // The coin type to transfer
@@ -92,7 +97,7 @@ const {
       [
         BCS.bcsToBytes(AccountAddress.fromHex('0x5964cbf9198fb32e601657b1e31eba60c227c682306b657ac16fb584feefca66')),
         // BCS.bcsToBytes(AccountAddress.fromHex('0xe02d26d9124f1086518bf4d1706505fc2dab6a2930c36c8ec46c3f4ed77eb186')),
-        BCS.bcsSerializeUint64(15),
+        BCS.bcsSerializeUint64(24),
       ],
     ),
   );
@@ -101,7 +106,7 @@ const {
   // const entryFunctionPayload = new TransactionPayloadEntryFunction(
   //   EntryFunction.natural(
   //     // Fully qualified module name, `AccountAddress::ModuleName`
-  //     "0xd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe::marketplace_instant_sale_example",
+  //     `${MARKET}::marketplace_v1`,
   //     // Module function
   //     "remove_listing",
   //     // The coin type to transfer
@@ -118,7 +123,7 @@ const {
   // const entryFunctionPayload = new TransactionPayloadEntryFunction(
   //   EntryFunction.natural(
   //     // Fully qualified module name, `AccountAddress::ModuleName`
-  //     "0xd2d2ef928344f9fece9716c0d4df28baa5f6a88d1f8985d99acec108d3b2e4fe::marketplace_instant_sale_example",
+  //     `${MARKET}::marketplace_v1`,
   //     // Module function
   //     "change_listing",
   //     // The coin type to transfer
